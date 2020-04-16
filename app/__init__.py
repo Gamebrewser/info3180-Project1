9d28353f219777283f2bd52d44a11f86aa9903e5
Gamebrewser <44047031+Gamebrewser@users.noreply.github.com>
2020-04-16 00:16:27 -0500

    final commit

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,7 @@ from subprocess import call
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "DontJustReadCodePracticeIt"
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://project1:password123@localhost/project1"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://blgcankvcgkgrj:5065cba8db3d49bb0c54e661f8bb1a9ee3c7d1dd0fc96b8b37d0ca2e384491e2@ec2-34-193-232-231.compute-1.amazonaws.com:5432/d204ebphoaom41"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # added just to suppress a warning
 app.config['UPLOAD_FOLDER'] = './app/static/photo'
 db = SQLAlchemy(app)
